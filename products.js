8e8a7622858f70f780241ef3babe4554de85e9a3
orangeappleak <appannagarikarthik.123@gmail.com>
2019-11-01 16:17:08 +0530

    completed adding products page , need to add the cart functionality to this page

--- products.js
+++ products.js
@@ -1,17 +1,23 @@
 (function() {
-    var products_div = document.querySelector(".products");
+    var products_div = document.querySelector("#bike_products");
     FirebaseService.getAllProducts().then(function(products){
-        var html = "";
+        var html = `<div id="overlay">
+                        <img id="overlay_back" style="width: 200%;top: -10px; border-radius: 100px;"src="unsplash%20walls/overlay_back.jpg">
+                        <div id="overlay_bike_products">
+                            <h1 id="overlay_heading">Mountain Bikes</h1>
+                        </div>
+                    </div>`;
         var count3=0;
         Object.keys(products).forEach(function(key){
-            html+=`<div class="product${count3}">
-<h1 class="product_heading">${products[key].name}</h1>
+            html+=`<div data-aos="fade-up" data-aos-delay="600" data-aos-duration="1000" class="product${count3}">
 <div class="images">
     <img class="product_image" src="${products[key].image}">
 </div>
 </div>`
+            document.getElementById("bike_products").innerHTML = html;
             count3++;
         })
-        document.getElementById("bikes").innerHTML = html;
     })
 })();
+
+
